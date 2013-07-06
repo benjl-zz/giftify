6ff253c876b2ea8640ff2b615f8c0ab837eafd26
Ben Cox <benjamin.jl.cox@gmail.com>
2013-07-06 15:08:17 -0400

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Giftify::Application.config.secret_token = 'dd4fd5c6f533eb48b39de1398f3377e1820d28b6f0bade514b2ed9b164371e26bf1fbd3ce0dcebc999493de257cfc38f5563469cab20b7720fd20bd40d7e1b00'
